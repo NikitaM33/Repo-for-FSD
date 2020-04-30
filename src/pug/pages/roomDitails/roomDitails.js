aaeb6991cc5ef6ce09a667cbefeac3f2e161bb1f
Nick <shynick33@hotmail.com>
2020-04-30 19:53:05 +0300

    adaptive 100%

--- src/pug/pages/roomDitails/roomDitails.js
+++ src/pug/pages/roomDitails/roomDitails.js
@@ -6,27 +6,33 @@ console.log('roomDitails page');
 // ============== Drop menu with botton
 
 $(document).ready(function() {
+  // ============== Burger menu
+  $('.header__burger').click(function() {
+    $('.header__burger, .header__nav').toggleClass('active');
+    $('body').toggleClass('lock')
+  });
 
-    $('.arrow-down').click(function() {
-      $(this).parent().next().slideToggle();
-    });
-  
-    // ============== Plus/minus buttons
-    $('.minus').click(function() {
-      var $label = $(this).parent().find('.quantity');
-      var count = parseInt($label.val()) - 1;
-      count = count < 0 ? 0 : count;
-      $label.val(count);
-      $label.change();
-      return false;
-    });
-  
-    $('.plus').click(function() {
-      var $label = $(this).parent().find('.quantity');
-      var count = parseInt($label.val()) + 1;
-      count = count > 9 ? 9 : count;
-      $label.val(count);
-      $label.change();
-      return false;
-    });
+  // ============== Toggle burger down
+  $('.arrow-down').click(function() {
+    $(this).parent().next().slideToggle();
+  });
+
+  // ============== Plus/minus buttons
+  $('.minus').click(function() {
+    var $label = $(this).parent().find('.quantity');
+    var count = parseInt($label.val()) - 1;
+    count = count < 0 ? 0 : count;
+    $label.val(count);
+    $label.change();
+    return false;
+  });
+
+  $('.plus').click(function() {
+    var $label = $(this).parent().find('.quantity');
+    var count = parseInt($label.val()) + 1;
+    count = count > 9 ? 9 : count;
+    $label.val(count);
+    $label.change();
+    return false;
+  });
 });
